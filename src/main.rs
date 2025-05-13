623af332ee7d5d189d0992867494db2ce7ccb6b4
xfnw <xfnw@riseup.net>
2025-05-13 19:30:59 -0400

    argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
+use clap::Parser;
+use std::{net::SocketAddr, path::PathBuf};
+
 mod request;
 mod response;
+
+#[derive(Debug, Parser)]
+struct Opt {
+    /// address to listen on
+    #[arg(long, default_value = "[::]:1965")]
+    bind: SocketAddr,
+    /// zip file to serve files from
+    /// 
+    /// defaults to the current binary, serving files from a zip file
+    /// concatenated with itself
+    #[arg(long, default_value = "/proc/self/exe")]
+    zip: PathBuf,
+    /// path to your tls certificate
+    cert: PathBuf,
+    /// path to your tls private key
+    /// 
+    /// defaults to looking in the same file as your certificate,
+    /// allowing both to be in one file
+    key: Option<PathBuf>,
+}
 
 #[derive(Debug, Eq, PartialEq, foxerror::FoxError)]
 enum Error {
@@ -13,4 +36,6 @@ enum Error {
     Userinfo,
 }
 
-fn main() {}
+fn main() {
+    let opt = Opt::parse();
+}
